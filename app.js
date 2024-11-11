4e6cb0991135a5e5cbb37cadd718c441ea112778
Gael GR <124109462+GaonaRiveraDiegoGael@users.noreply.github.com>
2024-11-11 00:54:39 +0000

    ☑ CLEAR: Parseo de peticiones entrantes

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-// Importando el modulo http
-import http from 'http';
 // Importando Express
 import express from 'express';
 
 // Creando la instancia de express
-// que basicamente es un middleware
 const app = express();
 
-// Ruta about
-// GET /about
-app.use('/about',(req, res)=>{
-  console.log("📢 Sirviendo la ruta '/about'");
-  // Se contesta al server
+// Middleware para manejar datos del formulario
+app.use(express.urlencoded({ extended: true })); // Procesa los datos del formulario
+
+// GET /add-product
+app.get('/add-product', (req, res, next) => {
+  // Si la petición es POST, pasa al siguiente middleware
+  if (req.method === "POST") return next();
+
+  // Sirve el formulario si la petición es GET
+  console.log("📢 Sirviendo formulario...");
   res.send(`
-    <h1>🪄 About...</h1>
-    <p>App for Fullstack Web Dev Course I!</p>
+    <form action="/add-product" method="POST">
+      <input type="text" name="title">
+      <button type="submit">Add product</button>
+    </form>
   `);
+});
+
+// POST /add-product
+app.post('/add-product', (req, res) => {
+  // Mostrar los datos del cuerpo de la solicitud (req.body)
+  console.log("📢 Datos recibidos:", req.body);
+
+  // Redirecciona a la página principal después de procesar la solicitud
+  res.redirect('/');
 });
 
 // Ruta Raíz
 // GET /
-app.use((req, res)=>{
+app.get('/', (req, res) => {
   console.log("📢 Sirviendo la ruta '/'");
-  // Se contesta al server
   res.send(`
     <h1>Welcome to Express Js</h1>
     <p>This is my awesome app! 😎</p>
+    <a href="/add-product">Go to Add Product</a>
   `);
 });
 
 // Definiendo puertos
 const port = 3000;
-const ip = "0.0.0.0"
+const ip = "0.0.0.0";
 
 // Arrancando el servidor
 app.listen(port, ip, () => {
