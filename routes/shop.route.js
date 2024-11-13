fe8a1ada3ccf2223a97ed3d2a1ff3636a2e063a2
Gael GR <124109462+GaonaRiveraDiegoGael@users.noreply.github.com>
2024-11-13 22:52:17 +0000

    ☑ CLEAR: S03A05 Creando vistas estáticas

---
+++ routes/shop.route.js
@@ -0,0 +1,32 @@
+// Importando el enrutador de express y el gestor de rutas
+import { Router } from 'express';
+import path from 'path';
+
+// Creando una instancia del enrutador de express
+const router = Router();
+
+// GET /
+router.get('/', (req, res) => {
+  console.log("📢 Sirviendo la ruta '/'");
+  // Enviar el archivo HTML 'shop.html' desde la carpeta 'views'
+  res.sendFile(path.resolve('views', 'shop.html'));
+});
+
+// GET /add-product
+router.get('/add-product', (req, res) => {
+  console.log("📢 Sirviendo la ruta '/add-product'");
+  // Enviar el archivo HTML 'add-product.html' desde la carpeta 'views'
+  res.sendFile(path.resolve('views', 'add-product.html'));
+});
+
+// GET /about
+router.get('/about', (req, res) => {
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Respuesta directa con HTML en línea
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
+
+export default router;
