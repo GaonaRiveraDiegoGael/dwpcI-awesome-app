df35a24d504c4e9f727cef588f106b44dd07b07f
Gael GR <124109462+GaonaRiveraDiegoGael@users.noreply.github.com>
2024-12-05 02:17:49 +0000

    ✅ S04A01 Servidor de archivos estáticos

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 // Importando Express
 import express from 'express';
+import httpStatus from 'http-status';
 
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
 
+// Importando el directorio raiz
+import { ROOT_DIR } from './helpers/paths.js'
+
+// Se importa path
+import path from 'path';
+
 // Creando la instancia de express
+// que basicamente es un middleware
 const app = express();
 
 // Se registra el middleware del body-parser
 app.use(express.urlencoded({ extended: true }));
 
-// Se agrega ruta de administrador
-app.use('/admin', adminRouter);
+// Se registra el middleware para el servidor
+// de archivos estaticos
+app.use(express.static(path.join(ROOT_DIR, 'public')));
 
 // Se agrega ruta de administrador
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
 
-// Registrando middleware para el error 404 sin http-status-codes
-app.use((req, res) => {
-  res.status(404).send(`
-    <h1 
-      style="color: crimson; text-align: center; font-size: 400%; margin: 3em 0 0 0">
-    🤷 404 RESOURCE NOT FOUND 🤷
-    </h1>
-  `);
+// Registrando el middleware para el error
+// 404
+app.use((req, res, next) => {
+  res.status(httpStatus.NOT_FOUND)
+  .sendFile(path.resolve('views','404.html'))
 });
 
 // Definiendo puertos
 const port = 3000;
-const ip = "0.0.0.0";
+const ip = "0.0.0.0"
 
 // Arrancando el servidor
 app.listen(port, ip, () => {
